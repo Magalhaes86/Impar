b441cbc750563d27839ae1a6e025be726f520e12
Marcos Magalhães <111365237+Magalhaes86@users.noreply.github.com>
2023-05-03 23:06:42 +0100

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,133 @@
+﻿
+namespace Impar
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.tbde = new System.Windows.Forms.TextBox();
+            this.tbpara = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.tbsms = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(439, 248);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(139, 46);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // tbde
+            // 
+            this.tbde.Location = new System.Drawing.Point(151, 50);
+            this.tbde.Name = "tbde";
+            this.tbde.Size = new System.Drawing.Size(156, 20);
+            this.tbde.TabIndex = 1;
+            this.tbde.Text = "+351910045307";
+            // 
+            // tbpara
+            // 
+            this.tbpara.Location = new System.Drawing.Point(156, 109);
+            this.tbpara.Name = "tbpara";
+            this.tbpara.Size = new System.Drawing.Size(150, 20);
+            this.tbpara.TabIndex = 2;
+            this.tbpara.Text = "+351963510569";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(63, 53);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(21, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "De";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(63, 116);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(29, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Para";
+            // 
+            // tbsms
+            // 
+            this.tbsms.Location = new System.Drawing.Point(156, 172);
+            this.tbsms.Multiline = true;
+            this.tbsms.Name = "tbsms";
+            this.tbsms.Size = new System.Drawing.Size(255, 167);
+            this.tbsms.TabIndex = 2;
+            this.tbsms.Text = "Teste Envio";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(63, 186);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(27, 13);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Sms";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(976, 576);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.tbsms);
+            this.Controls.Add(this.tbpara);
+            this.Controls.Add(this.tbde);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox tbde;
+        private System.Windows.Forms.TextBox tbpara;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox tbsms;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Newtonsoft.Json;
+using System.Text.Json;
+namespace Impar
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+       
+
+        private async Task<bool> assinc2(string from, string to, string content)
+        {
+            try
+            {
+                var client = new HttpClient();
+                client.DefaultRequestHeaders.Add("x-api-key", "gOhTfeXsLXdN7V-0Sct_GfYxROcqW1iK5JjmmvM3iOaFLkdSlJRFjrvTJQk-g_sb");
+
+                var response = await client.PostAsync(
+                    "https://api.httpsms.com/v1/messages/send",
+                    new StringContent(
+                        System.Text.Json.JsonSerializer.Serialize(new
+                        {
+                            from = tbde.Text,
+                            To = tbpara.Text,
+                            Content = tbsms.Text
+                        }),
+                        Encoding.UTF8,
+                        "application/json"
+                    )
+                );
+
+                return response.IsSuccessStatusCode;
+            }
+            catch (Exception ex)
+            {
+                // trate a exceção aqui, se necessário
+                return false;
+            }
+        }
+
+        private async void button1_Click(object sender, EventArgs e)
+        {
+            button1.Enabled = false;
+            try
+            {
+                var enviadoComSucesso = await assinc2(tbde.Text, tbpara.Text, tbsms.Text);
+                if (enviadoComSucesso)
+                {
+                    MessageBox.Show("SMS enviado com sucesso!");
+                    //tbde.Clear();
+                    //tbpara.Clear();
+                    //tbsms.Clear();
+                }
+                else
+                {
+                    MessageBox.Show("Falha ao enviar SMS!");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Erro ao enviar SMS: {ex.Message}");
+            }
+            finally
+            {
+                button1.Enabled = true;
+            }
+
+          
+        }
+    }
+}
